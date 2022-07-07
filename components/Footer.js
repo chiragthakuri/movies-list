af7b2545eef4d930777d4febe7ae333341f2d27c
chiragthakuri <83844592+chiragthakuri@users.noreply.github.com>
2022-07-07 12:04:36 -0500

    added main layout compoent

---
+++ components/Footer.js
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return ( 
+    <div>
+        <p>Copyright 2022 Movies List</p>
+    </div> );
+}
+ 
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,14 @@
+const About = () => {
+  return (  
+  <div>
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio aliquam aliquid, voluptatum repellat non atque, provident debitis dolorem officiis necessitatibus architecto ad cupiditate rem nihil ab quo unde, minus amet!
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio aliquam aliquid, voluptatum repellat non atque, provident debitis dolorem officiis necessitatibus architecto ad cupiditate rem nihil ab quo unde, minus amet!    </p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio aliquam aliquid, voluptatum repellat non atque, provident debitis dolorem officiis necessitatibus architecto ad cupiditate rem nihil ab quo unde, minus amet!    </p>
+    
+    </p>
+
+  </div>);
+}
+
+export default About;
